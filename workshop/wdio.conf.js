91ed439c3dc3dda55d462c176c66cbf559b0cf35
Manuel Zambrano <dev.mzambrano@gmail.com>
2021-01-27 17:39:22 +0100

    html reporter

--- workshop/wdio.conf.js
+++ workshop/wdio.conf.js
@@ -1,3 +1,5 @@
+const AllureService = require('./services/allure-service')
+
 exports.config = {
     //
     // ====================
@@ -106,7 +108,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: [[AllureService, {}]],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
